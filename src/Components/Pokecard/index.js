ed49234f67115cae1399a9e026375b497798dd4d
Fatali Fataliyev <fataliyev999@gmail.com>
2022-08-12 06:23:47 +0400

    Pokecard & Pokedex components created

---
+++ src/Components/Pokecard/index.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+export default class Pokecard extends Component(){
+
+    constructor(){
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return('');
+    }
+}
---
+++ src/Components/Pokedex/index.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+export default class Pokedex extends Component(){
+
+    constructor(){
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return('');
+    }
+}
